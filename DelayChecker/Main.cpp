a7b127e82c6a897c2a87a350ebd9bd76b31c0448
Semyon Labzov <s-labzov@armo.ru>
2021-10-25 15:18:16 +0300

    - rtsp input arg parse

--- DelayChecker/Main.cpp
+++ DelayChecker/Main.cpp
@@ -1,6 +1,9 @@
 #include <opencv2/opencv.hpp>
 #include <ctime>
 #include <format>
+#include <Windows.h>
+#include <atlconv.h>
+#include <atlsafe.h>
 
 
 void draw_time(const cv::Mat& image)
@@ -24,12 +27,25 @@ void draw_time(const cv::Mat& image)
 	cv::putText(image, str, point, cv::FONT_HERSHEY_DUPLEX, font_scale, cv::Scalar(255, 255, 255), 1, cv::LINE_AA );
 }
 
-int main()
+std::string parse_cmd_args(const LPWSTR& lpCmdLine)
 {
-	auto conn_str = "rtsp://localhost:5555/stream";
+	auto args_count = 0;
+	const auto* sz_arg_list = CommandLineToArgvW(lpCmdLine, &args_count);
+	auto conn_str = std::string(ATL::CW2A(sz_arg_list[0]));
+
+	return conn_str.find("DelayChecker.exe") != std::string::npos ? "rtsp://localhost:5555/stream" : conn_str;
+}
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                      _In_opt_ HINSTANCE hPrevInstance,
+                      _In_ LPWSTR lpCmdLine,
+                      _In_ int nCmdShow)
+{
+	auto conn_str = parse_cmd_args(lpCmdLine);
 	cv::Mat image;
 	cv::VideoCapture reader(conn_str);
 	cv::namedWindow(conn_str, cv::WINDOW_AUTOSIZE);
+	const int NEXT_FRAME_TIME = 34;
 
 	while( cv::getWindowProperty(conn_str, cv::WindowPropertyFlags::WND_PROP_FULLSCREEN) != -1 )
 	{
@@ -38,10 +54,10 @@ int main()
 		
 		draw_time(image);
 		cv::imshow(conn_str, image);
-		cv::waitKey(34);
+		cv::waitKey(NEXT_FRAME_TIME);
 		image.release();
 	}
 
 	reader.release();
-	return 0;
+	ExitProcess(0);
 }
