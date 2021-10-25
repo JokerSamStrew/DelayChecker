c5fe94cfaf6894ebd5df27b32fbc1733d383e39b
Semyon Labzov <s-labzov@armo.ru>
2021-10-25 17:16:49 +0300

    - minor changes

--- DelayChecker/Main.cpp
+++ DelayChecker/Main.cpp
@@ -5,6 +5,7 @@
 #include <atlsafe.h>
 
 
+
 void draw_time(const cv::Mat& image)
 {
 	tm newtime;
@@ -14,7 +15,7 @@ void draw_time(const cv::Mat& image)
 	if (err)
 		str = "locatimme err";
 
-	auto hour = newtime.tm_hour < 10 ? "0" + std::to_string(newtime.tm_hour) : std::to_string(newtime.tm_hour);
+	auto hour = newtime.tm_hour < 10 ? " " + std::to_string(newtime.tm_hour) : std::to_string(newtime.tm_hour);
 	auto min = newtime.tm_min < 10 ? "0" + std::to_string(newtime.tm_min) : std::to_string(newtime.tm_min);
 	auto sec = newtime.tm_sec < 10 ? "0" + std::to_string(newtime.tm_sec) : std::to_string(newtime.tm_sec);
 
@@ -26,11 +27,13 @@ void draw_time(const cv::Mat& image)
 	cv::putText(image, str, point, cv::FONT_HERSHEY_DUPLEX, font_scale, cv::Scalar(255, 255, 255), 1, cv::LINE_AA );
 }
 
-std::string parse_cmd_args(const LPWSTR& lpCmdLine)
+std::string parse_cmd_args(const LPWSTR& lpCmdLine, int& next_frame_time)
 {
 	auto args_count = 0;
 	const auto* sz_arg_list = CommandLineToArgvW(lpCmdLine, &args_count);
 	auto conn_str = std::string(ATL::CW2A(sz_arg_list[0]));
+	if (args_count == 2)
+		next_frame_time = atoi(ATL::CW2A(sz_arg_list[1]));
 
 	return conn_str.find("DelayChecker.exe") != std::string::npos ? "rtsp://localhost:5555/stream" : conn_str;
 }
@@ -40,20 +43,24 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
                       _In_ LPWSTR lpCmdLine,
                       _In_ int nCmdShow)
 {
-	auto conn_str = parse_cmd_args(lpCmdLine);
+	int next_frame_time = 32;
+	auto conn_str = parse_cmd_args(lpCmdLine, next_frame_time);
 	cv::Mat image;
 	cv::VideoCapture reader(conn_str);
+	std::cout << reader.getBackendName() << std::endl;
+	std::cout << ( reader.getExceptionMode() ? "Exception mode is active" : "Exception mode is not active" ) << std::endl;
 	cv::namedWindow(conn_str, cv::WINDOW_AUTOSIZE);
-	const int NEXT_FRAME_TIME = 34;
 
 	while( cv::getWindowProperty(conn_str, cv::WindowPropertyFlags::WND_PROP_FULLSCREEN) != -1 )
 	{
-		if (!reader.read(image))
+		if (!reader.read(image)){
+			reader.grab();
 			continue;
+		}
 		
 		draw_time(image);
 		cv::imshow(conn_str, image);
-		cv::waitKey(NEXT_FRAME_TIME);
+		cv::waitKey(next_frame_time);
 		image.release();
 	}
 
