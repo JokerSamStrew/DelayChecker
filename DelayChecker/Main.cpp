ba9afc7a162ae75819fc253eb48242fe7f3293a6
Semyon Labzov <s-labzov@armo.ru>
2021-10-25 13:12:40 +0300

    - 30 frames per second display - close if press close button - print current time

--- DelayChecker/Main.cpp
+++ DelayChecker/Main.cpp
@@ -1,23 +1,47 @@
 #include <opencv2/opencv.hpp>
-#include <Windows.h>
+#include <ctime>
+#include <format>
 
+
+void draw_time(const cv::Mat& image)
+{
+	tm newtime;
+	const time_t now = time(NULL);
+	errno_t err = localtime_s(&newtime, &now);
+	std::string str;
+	if (err)
+		str = "locatimme err";
+
+	auto hour = newtime.tm_hour < 10 ? "0" + std::to_string(newtime.tm_hour) : std::to_string(newtime.tm_hour);
+	auto min = newtime.tm_min < 10 ? "0" + std::to_string(newtime.tm_min) : std::to_string(newtime.tm_min);
+	auto sec = newtime.tm_sec < 10 ? "0" + std::to_string(newtime.tm_sec) : std::to_string(newtime.tm_sec);
+
+	str = hour + ":" + min + ":" + sec;
+
+	auto point = cv::Point(20, 40 + 30);
+	auto font_scale = 1.1;
+	cv::putText(image, str, point, cv::FONT_HERSHEY_DUPLEX, font_scale, cv::Scalar(0, 0, 0), 6, cv::LINE_AA);
+	cv::putText(image, str, point, cv::FONT_HERSHEY_DUPLEX, font_scale, cv::Scalar(255, 255, 255), 1, cv::LINE_AA );
+}
 
 int main()
 {
 	auto conn_str = "rtsp://localhost:5555/stream";
 	cv::Mat image;
 	cv::VideoCapture reader(conn_str);
+	cv::namedWindow(conn_str, cv::WINDOW_AUTOSIZE);
 
-	while (!reader.retrieve(image) || image.empty())
+	while( cv::getWindowProperty(conn_str, cv::WindowPropertyFlags::WND_PROP_FULLSCREEN) != -1 )
 	{
-		reader.grab();
+		if (!reader.read(image))
+			continue;
+		
+		draw_time(image);
+		cv::imshow(conn_str, image);
+		cv::waitKey(34);
+		image.release();
 	}
 
 	reader.release();
-
-	cv::namedWindow(conn_str, cv::WINDOW_AUTOSIZE);
-	cv::imshow(conn_str, image);
-
-	cv::waitKey(10000);
 	return 0;
 }
